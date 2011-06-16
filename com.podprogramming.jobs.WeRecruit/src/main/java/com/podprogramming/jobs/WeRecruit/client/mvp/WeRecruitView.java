615a2c4af2465519b38861d969c1dae90bb3c20d
xavier.seignard <xavier.seignard@pod-programming.com>
2011-06-16 11:05:21 +0200

    Styling and incentive

--- com.podprogramming.jobs.WeRecruit/src/main/java/com/podprogramming/jobs/WeRecruit/client/mvp/WeRecruitView.java
+++ com.podprogramming.jobs.WeRecruit/src/main/java/com/podprogramming/jobs/WeRecruit/client/mvp/WeRecruitView.java
@@ -1,13 +1,13 @@
 package com.podprogramming.jobs.WeRecruit.client.mvp;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHTML;
 import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RadioButton;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * A view which implements the IWeRecruitActivity.IWeRecruitView interface in
@@ -38,80 +38,81 @@ public class WeRecruitView extends Composite implements IWeRecruitActivity.IWeRe
 	private final RadioButton q4Answer3;
 	
 	public WeRecruitView() {
-		VerticalPanel viewContainer = new VerticalPanel();
+		//VerticalPanel viewContainer = new VerticalPanel();
+		FlowPanel viewContainer = new FlowPanel();
 		viewContainer.setStylePrimaryName( "weRecruitView-main" );
 		
 		// Question 1
 		q1 = new Label("Quelle annotation existe dans Guice?");
-		q1.setStylePrimaryName( "weRecruitView-item" );
+		q1.setStylePrimaryName( "weRecruitView-question" );
 		viewContainer.add(q1);
 		
 		q1Answer1 = new RadioButton("q1", "@Inject");
-		q1Answer1.setStylePrimaryName( "weRecruitView-item" );
+		q1Answer1.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q1Answer1 );
 		
 		q1Answer2 = new RadioButton("q1", "@Injection");
-		q1Answer2.setStylePrimaryName( "weRecruitView-item" );
+		q1Answer2.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q1Answer2 );
 		
 		q1Answer3 = new RadioButton("q1", "@Deprecated");
-		q1Answer3.setStylePrimaryName( "weRecruitView-item" );
+		q1Answer3.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q1Answer3 );
 		
 		// Question 2
 		q2 = new Label("SaaS veut dire");
-		q2.setStylePrimaryName( "weRecruitView-item" );
+		q2.setStylePrimaryName( "weRecruitView-question" );
 		viewContainer.add(q2);
 		
 		q2Answer1 = new RadioButton("q2", "Software as a Service");
-		q2Answer1.setStylePrimaryName( "weRecruitView-item" );
+		q2Answer1.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q2Answer1 );
 		
 		q2Answer2 = new RadioButton("q2", "System as a Service");
-		q2Answer2.setStylePrimaryName( "weRecruitView-item" );
+		q2Answer2.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q2Answer2 );
 		
 		q2Answer3 = new RadioButton("q2", "Obiwan Kenobi");
-		q2Answer3.setStylePrimaryName( "weRecruitView-item" );
+		q2Answer3.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q2Answer3 );
 		
 		// Question 3
 		q3 = new Label("double number = (Double)null;");
-		q3.setStylePrimaryName( "weRecruitView-item" );
+		q3.setStylePrimaryName( "weRecruitView-question" );
 		viewContainer.add(q3);
 		
 		q3Answer1 = new RadioButton("q3", "Ne compile pas.");
-		q3Answer1.setStylePrimaryName( "weRecruitView-item" );
+		q3Answer1.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q3Answer1 );
 		
 		q3Answer2 = new RadioButton("q3", "Renvoie une NumberFormatException à l'éxécution.");
-		q3Answer2.setStylePrimaryName( "weRecruitView-item" );
+		q3Answer2.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q3Answer2 );
 		
 		q3Answer3 = new RadioButton("q3", "Renvoie une NullPointerException à l'exécution.");
-		q3Answer3.setStylePrimaryName( "weRecruitView-item" );
+		q3Answer3.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q3Answer3 );
 		
 		// Question 4
 		q4 = new Label("GoF signifie :");
-		q4.setStylePrimaryName( "weRecruitView-item" );
+		q4.setStylePrimaryName( "weRecruitView-question" );
 		viewContainer.add(q4);
 		
 		q4Answer1 = new RadioButton("q4", "Getter on Field");
-		q4Answer1.setStylePrimaryName( "weRecruitView-item" );
+		q4Answer1.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q4Answer1 );
 		
 		q4Answer2 = new RadioButton("q4", "Gang of Four");
-		q4Answer2.setStylePrimaryName( "weRecruitView-item" );
+		q4Answer2.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q4Answer2 );
 		
 		q4Answer3 = new RadioButton("q4", "Good old Frame");
-		q4Answer3.setStylePrimaryName( "weRecruitView-item" );
+		q4Answer3.setStylePrimaryName( "weRecruitView-answer" );
 		viewContainer.add( q4Answer3 );
 		
 		// the label where the information is combined
 		mailTo = new HTML();
-		mailTo.setStylePrimaryName( "weRecruitView-mailToHTML" );
+		mailTo.setStylePrimaryName( "weRecruitView-mail" );
 		viewContainer.add( mailTo );
 
 		// defines the container as the composite widget
