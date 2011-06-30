4b20bbf9c6c36af9fef9933046d20ee3a5641960
unknown <Sbastien@.(none)>
2011-06-30 16:54:08 +0200

    maven2gradle :-)

--- com.podprogramming.jobs.WeRecruit/old/client/mvp/IWeRecruitHistoryMapper.java
+++
@@ -1,8 +0,0 @@
-package com.podprogramming.jobs.WeRecruit.client.mvp;
-
-import com.google.gwt.place.shared.PlaceHistoryMapper;
-import com.google.gwt.place.shared.WithTokenizers;
-
-@WithTokenizers( {WeRecruitPlace.Tokenizer.class} )
-public interface IWeRecruitHistoryMapper extends PlaceHistoryMapper {
-}
